f71f4ab36cb33f2a023fba8fe05d3e7a1b6daab4
yolajyan <120991194+yolajyan@users.noreply.github.com>
2023-01-14 21:32:17 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        IDandPasswords idandPasswords = new IDandPasswords();
+//
+//        Login logInPage = new Login(idandPasswords.getLoginInfo());
+//
+//    }
+//}
