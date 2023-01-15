72d19e49e70e445ffe7870f4e560c15ad4f56718
yolajyan <120991194+yolajyan@users.noreply.github.com>
2023-01-14 21:37:04 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        IDandPasswords idandPasswords = new IDandPasswords();
-//
-//        Login logInPage = new Login(idandPasswords.getLoginInfo());
-//
-//    }
-//}
